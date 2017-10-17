69d101774d694df2ce27bcc0b9fc27a8f303031b
gaurav8758 <gaurav8758@gmail.com>
2017-10-17 13:34:36 -0500

    Update Extent Manager path location

--- src/main/java/com/projectname/qa/util/ExtentManager.java
+++ src/main/java/com/projectname/qa/util/ExtentManager.java
@@ -1,6 +1,8 @@
 package com.projectname.qa.util;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.projectname.qa.base.TestBase;
 import com.relevantcodes.extentreports.DisplayOrder;
@@ -9,20 +11,21 @@ import com.relevantcodes.extentreports.ExtentReports;
 public class ExtentManager {
     static ExtentReports extent;
     //final static String filePath = System.getProperty("user.dir") + "\\test-output\\ExtentReport.html";
-    final static String filePath = TestBase.GlobalExtentReportsLocation;
+    static Path filePath;
     
     public synchronized static ExtentReports getReporter() {
     	//System.out.println("filepath:" + filePath);
+    	filePath = Paths.get(TestBase.GlobalExtentReportsLocation);
         if (extent == null) {
         	if (TestBase.GlobalExtentReportsOverWrite)
         	{
-        		extent = new ExtentReports(filePath, true, DisplayOrder.OLDEST_FIRST);
+        		extent = new ExtentReports(filePath.toString(), true, DisplayOrder.OLDEST_FIRST);
         		System.out.println(filePath);
         		System.out.println("inside true");
         	}
         	else
         	{
-        		extent = new ExtentReports(filePath, false, DisplayOrder.OLDEST_FIRST);
+        		extent = new ExtentReports(filePath.toString(), false, DisplayOrder.OLDEST_FIRST);
         		System.out.println("inside false");
         	}
         	extent
