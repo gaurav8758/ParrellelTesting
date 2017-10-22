0a250b683f26b826cf35ce61e9f72bfccf2322fd
gaurav8758 <gaurav8758@gmail.com>
2017-10-21 22:12:57 -0500

    Customized test base for desktop only code

--- src/main/java/com/projectname/qa/base/TestBase.java
+++ src/main/java/com/projectname/qa/base/TestBase.java
@@ -71,12 +71,16 @@ public class TestBase {
 		e_driver.register(eventListener);
 		driver = e_driver;
 		
-		//driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		if (Globalplatform.equalsIgnoreCase("desktop"))
+		{
+			//driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+			
+	        driver.get(TestBase.GlobalURL);
+		}
 
-        driver.get(GlobalURL);
 		return driver;
     }
     
--- src/main/java/com/projectname/qa/util/LocalDriverFactory.java
+++ src/main/java/com/projectname/qa/util/LocalDriverFactory.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,6 +12,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 import com.projectname.qa.base.TestBase;
 
 public class LocalDriverFactory {
